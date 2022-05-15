4f733a9b89282a7293f0440a46c6ef12fe47db9c
guymforer <guymforer@gmail.com>
2022-05-15 18:12:50 +0300

    adding files to the project

---
+++ src/main/java/model/TimeSeriesAnomalyDetector.java
@@ -0,0 +1,17 @@
+//interface- algorithms implements
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+import javafx.scene.chart.XYChart.Series;
+import view.TimeSeries;
+
+public interface TimeSeriesAnomalyDetector {
+
+    public void learnNormal(TimeSeries ts);
+    public List<AnomalyReport> detect(TimeSeries ts);
+    public Series  paint(String ...strings );
+    public String getName();
+}
--- src/main/java/view/graphs/Graphs.java
+++ src/main/java/view/graphs/Graphs.java
@@ -43,7 +43,7 @@ public class Graphs extends AnchorPane
             this.getChildren().add(ap);
         }
         else
-            controller=null;
+            controller = null;
     }
 
 }
